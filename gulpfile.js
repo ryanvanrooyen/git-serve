7fdf568777083ec22f6b72dd7a0897fd1572cead
Ryan <ryan.vanrooyen@icloud.com>
2016-02-28 22:15:23 -0700

    Updating package json to support running from command line.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 
 /* global process */
 var gulp = require('gulp');
+var insert = require('gulp-insert');
 // var ts = require('gulp-typescript');
 // var mocha = require('gulp-mocha');
 // var plumber = require('gulp-plumber');
 // var merge = require('merge2');
-// var insert = require('gulp-insert');
 // var sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('release', function () {
 
 
+});
+
+gulp.task('release', function () {
+
+	return gulp.src('Source/cli.js')
+		.pipe(insert.prepend('#! /usr/bin/env node \n'))
+		.pipe(gulp.dest('./Bin'));
 });
 
 gulp.task('watch', function () {
